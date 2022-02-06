d2a85210a7a4b435ba3dc7d6b879c637904110f7
Sloan Lipman <sloan.lipman@gmail.com>
2022-02-06 12:40:54 -0500

    Fix up build, add some dependencies

---
+++ src/main/java/com/slipman/assessment/controller/HatchawaysAssessementController.java
@@ -0,0 +1,13 @@
+package com.slipman.assessment.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HatchawaysAssessementController
+{
+
+    // @GetMapping("/ping")
+    // public ResponseEntity<Map<String, Object>>
+}
---
+++ src/main/java/com/slipman/assessment/domain/Ping.java
@@ -0,0 +1,6 @@
+package com.slipman.assessment.domain;
+
+public class Ping
+{
+    private boolean success;
+}
