1164368b7fa7eadb3043d05d36fd9222b1805c8b
Sloan Lipman <sloan.lipman@gmail.com>
2022-02-06 11:29:26 -0500

    Set up initial project

---
+++ src/main/java/com/slipman/assessment/HatchawaysAssessementApplication.java
@@ -0,0 +1,13 @@
+package com.slipman.assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HatchawaysAssessementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HatchawaysAssessementApplication.class, args);
+	}
+
+}
