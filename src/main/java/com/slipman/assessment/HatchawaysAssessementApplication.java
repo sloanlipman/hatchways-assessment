a6e21026c76da29a256a11d47e994982bdbef358
Sloan Lipman <sloan.lipman@gmail.com>
2022-02-06 12:15:40 -0500

    Add Spotless, fix build

--- src/main/java/com/slipman/assessment/HatchawaysAssessementApplication.java
+++ src/main/java/com/slipman/assessment/HatchawaysAssessementApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HatchawaysAssessementApplication {
+public class HatchawaysAssessementApplication
+{
 
-	public static void main(String[] args) {
-		SpringApplication.run(HatchawaysAssessementApplication.class, args);
-	}
-
+    public static void main(String[] args)
+    {
+        SpringApplication.run(HatchawaysAssessementApplication.class, args);
+    }
 }
